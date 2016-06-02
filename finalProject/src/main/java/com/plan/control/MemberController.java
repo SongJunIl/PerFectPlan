3544e4bce6c45b758050a1f135bb2a8bc45bcac3
USER <USER@USER-PC>
2016-06-02 09:44:48 +0900

    test

---
+++ finalProject/src/main/java/com/plan/control/MemberController.java
@@ -0,0 +1,16 @@
+package com.plan.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
+public class MemberController {
+
+	@RequestMapping("mypage")
+	public void mypage(){
+		
+	}
+	
+	
+}

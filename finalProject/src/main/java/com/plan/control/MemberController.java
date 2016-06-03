4c71ae0b1853ede4200f743e87b4745c8e9d9e33
USER <USER@USER-PC>
2016-06-03 09:59:00 +0900

    test/0603/10:00

--- finalProject/src/main/java/com/plan/control/MemberController.java
+++ finalProject/src/main/java/com/plan/control/MemberController.java
@@ -3,18 +3,7 @@ package com.plan.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("member")
-public class MemberController {
-
-	@RequestMapping("mypage")
-	public void mypage(){
-		
-	}
-=======
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
